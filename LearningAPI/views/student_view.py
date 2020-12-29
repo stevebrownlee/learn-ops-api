0b369e8268e48799a19fbf8259914060fa55db99
Steve Brownlee <chortlehoort@gmail.com>
2020-12-29 10:40:55 -0600

    Delete student

--- LearningAPI/views/student_view.py
+++ LearningAPI/views/student_view.py
@@ -64,7 +64,17 @@ class StudentViewSet(ViewSet):
         Returns:
             Response -- 200, 404, or 500 status code
         """
-        return Response({}, status=status.HTTP_200_OK)
+        try:
+            event = NssUser.objects.get(pk=pk)
+            event.delete()
+
+            return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+        except NssUser.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def list(self, request):
         """Handle GET requests for all items
