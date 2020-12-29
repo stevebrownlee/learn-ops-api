8e524ad8438d153829f3b371fec1baa3eecefb67
Steve Brownlee <chortlehoort@gmail.com>
2020-12-29 10:39:20 -0600

    Get single student Only get a single student if auth user is the student or is staff

--- LearningAPI/views/student_view.py
+++ LearningAPI/views/student_view.py
@@ -1,11 +1,11 @@
 from rest_framework import permissions
-from rest_framework.decorators import permission_classes
+from django.http import HttpResponseServerError
 from rest_framework import serializers
 from rest_framework import status
+from django.db.models import Q
 from rest_framework.viewsets import ViewSet
-from rest_framework.permissions import IsAdminUser
-from LearningAPI.models import NssUser
 from rest_framework.response import Response
+from LearningAPI.models import NssUser
 
 class StudentPermission(permissions.BasePermission):
 
@@ -27,16 +27,8 @@ class StudentViewSet(ViewSet):
 
 
     def create(self, request):
-        """Handle POST operations
-
-        Returns:
-            Response -- JSON serialized instance
-        """
-        students = NssUser.objects.filter(user__is_staff=False)
-
-        serializer = StudentSerializer(
-            students, many=True, context={'request': request})
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        """Handle POST operations"""
+        return Response(None, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def retrieve(self, request, pk=None):
         """Handle GET requests for single item
@@ -44,7 +36,19 @@ class StudentViewSet(ViewSet):
         Returns:
             Response -- JSON serialized instance
         """
-        return Response({}, status=status.HTTP_200_OK)
+        try:
+            student = NssUser.objects.get(pk=pk)
+            if request.auth.user == student.user or request.auth.user.is_staff:
+                serializer = StudentSerializer(student, context={'request': request})
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(None, status=status.HTTP_401_UNAUTHORIZED)
+
+        except NssUser.DoesNotExist as ex:
+            return Response(None, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return HttpResponseServerError(ex)
 
     def update(self, request, pk=None):
         """Handle PUT requests
