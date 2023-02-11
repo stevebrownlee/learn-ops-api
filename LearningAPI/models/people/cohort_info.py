c5972ad4e6c484d04b7f0cbae42465c5a578b54e
Steve <chortlehoort@gmail.com>
2023-02-11 10:29:51 -0600

    Removed conflicting migrations

--- LearningAPI/models/people/cohort_info.py
+++ LearningAPI/models/people/cohort_info.py
@@ -3,6 +3,6 @@ from django.db import models
 class CohortInfo(models.Model):
     """Additional, non-critical information for a cohort."""
     cohort = models.OneToOneField("Cohort", on_delete=models.CASCADE, related_name="info")
-    student_organization_url = models.CharField(max_length=75, unique=True)
-    github_classroom_url = models.CharField(max_length=75, unique=True)
-    attendance_sheet_url = models.CharField(max_length=255, unique=True)
+    student_organization_url = models.CharField(max_length=75, unique=True, null=True, blank=True)
+    github_classroom_url = models.CharField(max_length=75, unique=True, null=True, blank=True)
+    attendance_sheet_url = models.CharField(max_length=255, unique=True, null=True, blank=True)
