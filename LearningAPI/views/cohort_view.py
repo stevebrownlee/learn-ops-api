6639a57bb3b9b4077c0113be94679b1ea094e9a6
Steve <chortlehoort@gmail.com>
2023-02-21 13:28:05 -0600

    member variable is needed

--- LearningAPI/views/cohort_view.py
+++ LearningAPI/views/cohort_view.py
@@ -251,8 +251,8 @@ class CohortViewSet(ViewSet):
 
                 if user_type is not None and user_type == "instructor":
                     try:
-                        nss_user = NssUser.objects.get(user=request.auth.user)
-                        membership = NssUserCohort.objects.get(nss_user=nss_user)
+                        member = NssUser.objects.get(user=request.auth.user)
+                        membership = NssUserCohort.objects.get(nss_user=member)
 
                         return Response(
                             {'message': f'Instructor cannot be in more than 1 cohort at a time. Currently assigned to cohort {membership.cohort.name}'},
