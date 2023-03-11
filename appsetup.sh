d640415517f948eb9fddee8bd7d26021e8c62ef3
Steve <chortlehoort@gmail.com>
2023-03-11 13:06:08 -0600

    More script changes for automation

--- appsetup.sh
+++ appsetup.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-sudo systemctl start postgresql.service
-
 set -e
+
+git clone https://github.com/stevebrownlee/learn-ops-api.git
+sudo apt update -y
+sudo apt install git curl python3-pip postgresql postgresql-contrib -y
+
+pip3 install -r requirements.txt
+
+
 
 sudo su - postgres <<EOF
 psql -c "DROP DATABASE IF EXISTS $LEARN_OPS_DB WITH (FORCE);"
 psql -c "CREATE DATABASE $LEARN_OPS_DB;"
+
 psql -c "CREATE USER $LEARN_OPS_USER WITH PASSWORD '$LEARN_OPS_PASSWORD';"
 psql -c "ALTER ROLE $LEARN_OPS_USER SET client_encoding TO 'utf8';"
 psql -c "ALTER ROLE $LEARN_OPS_USER SET default_transaction_isolation TO 'read committed';"
 psql -c "ALTER ROLE $LEARN_OPS_USER SET timezone TO 'UTC';"
 psql -c "GRANT ALL PRIVILEGES ON DATABASE $LEARN_OPS_DB TO $LEARN_OPS_USER;"
 EOF
+
+pip install -r requirements.txt
+
--- seed.sh
+++ seed.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Dump the existing database
+set -e
+sudo su - postgres <<EOF
 psql -U $LEARN_OPS_USER $LEARN_OPS_DB -t -c "select 'drop table \"' || tablename || '\" cascade;' from pg_tables where schemaname = 'public'"  | psql -U $LEARN_OPS_USER $LEARN_OPS_DB
+EOF
 
-# Delete existing migrations and create DB
-# rm -rf ./LearningAPI/migrations
+# Run existing migrations
 python3 manage.py migrate
-# python3 manage.py makemigrations LearningAPI
-# python3 manage.py migrate LearningAPI
 
 # Load data from backup
 python3 manage.py loaddata socialaccount
